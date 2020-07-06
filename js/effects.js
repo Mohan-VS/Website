e4bee3b6c4553b1a2cb2b39957c1ea069a0afd49
Mohan V S <53256123+MohanVallivedu@users.noreply.github.com>
2020-07-06 12:16:51 -0500

    Revert "Fix hamburger icon issue"

--- js/effects.js
+++
@@ -1,8 +0,0 @@
-$('button.btn.btn-block').on("click", () => {
-    const linksDiv = $('div.links');
-    if (linksDiv.hasClass('hidden')) {
-        linksDiv.removeClass('hidden')
-    } else {
-        linksDiv.addClass('hidden')
-    }
-});
